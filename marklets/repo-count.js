9565ae8acb59b51b22d1f0e8e53719e6c2ebe121
Will Anderson <will@itsananderson.com>
2014-08-28 13:23:49 -0700

    Remove calls to console.log

--- marklets/repo-count.js
+++ marklets/repo-count.js
@@ -11,9 +11,7 @@ javascript:(function() {
     function repoCount() {
         $('.js-repo-filter-tab').each(function() {
             var $this = $(this);
-            console.log($this.attr('data-filter'));
             var repolist = $('.repo-list, #repo_listing').find($this.attr('data-filter'));
-            console.log(repolist);
             $this.html($this.text() + '<p style="text-align: center; margin: 0">(' + repolist.length + ')</p>');
         });
         $('.repo_filterer').css("margin-top", 0);
